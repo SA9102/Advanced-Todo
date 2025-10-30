7b5f286460d03db14e6c4cc2ec737883fc3785d6
ec23052 <ec23052@qmul.ac.uk>
2025-10-30 00:30:36 +0000

    HomePage.tsx: refactoring and cleaning

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL = "http://localhost:3000/api";
-// "https://advanced-todo-backend-j6a4.onrender.com/api";
+export const API_BASE_URL =
+  "https://advanced-todo-backend-j6a4.onrender.com/api";
+// "http://localhost:3000/api";
